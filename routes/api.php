1f98cd5bd450759ef63366c34dc5096327e0ba8a
Dylan Pierce <me@dylanjpierce.com>
2016-12-16 16:33:23 -0500

    Adding routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::group(['namespace' => 'Api'], function() {
+	Route::post('/product', 'ProductController@store');
+	Route::get('/product', 'ProductController@listing');
+});
